0e47ed784a5d9b1b57f513ab3c632bafce2f690d
Pablo <panicoper@gmail.com>
2015-11-17 13:28:41 -0300

    fix assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( alimentos_p.jpg )
 Rails.application.config.assets.precompile += %w( logo/spree_50.png )
 # Rails.application.config.assets.precompile += %w( alimentos_p.jpg )
-Rails.application.config.assets.precompile = true
+# Rails.application.config.assets.precompile = true
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
